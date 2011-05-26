2e13d68d9effc6a19eef2e1be8614f757fd97e05
Daniel Hiller <dhiller@gmx.de>
2011-05-26 08:10:44 +0200

    Sourcecodes auf UTF-8 Encoding eingestellt

--- application/src/de/jugmuenster/android/updates/item/HttpURIContentProvider.java
+++ application/src/de/jugmuenster/android/updates/item/HttpURIContentProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, Java User Group Münster, NRW, Germany, 
+ * Copyright (c) 2011, Java User Group MÃ¼nster, NRW, Germany, 
  * http://www.jug-muenster.de
  * All rights reserved.
  * 
@@ -11,7 +11,7 @@
  *  - 	Redistributions in binary form must reproduce the above copyright notice, 
  * 	this list of conditions and the following disclaimer in the documentation 
  * 	and/or other materials provided with the distribution.
- *  - 	Neither the name of the Java User Group Münster nor the names of its contributors may 
+ *  - 	Neither the name of the Java User Group MÃ¼nster nor the names of its contributors may 
  * 	be used to endorse or promote products derived from this software without 
  * 	specific prior written permission.
  * 
